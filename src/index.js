aed4ddd656ca45e4d8ce262518fe5f637d1cd057
Pawan Jain <jainpawan211199@gmail.com>
2020-01-02 11:37:56 +0530

    initial socket.io

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const http = require('http')
 const path  = require('path')
+const socketio = require('socket.io')
+
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 
@@ -8,6 +13,10 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', ()=> {
+  console.log('New WebSocket Connection')
+})
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}!`)
 })
