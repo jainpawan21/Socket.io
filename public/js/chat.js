dda9e62c437a89122f886df04c3e523b8cf8ab49
Pawan Jain <jainpawan211199@gmail.com>
2020-01-02 14:31:32 +0530

    Rendering location message

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,8 @@ const $messages = document.querySelector('#messages')
 
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+
 
 socket.on('message', (message)=> {
   console.log(message)
@@ -18,6 +20,15 @@ socket.on('message', (message)=> {
   })
   $messages.insertAdjacentHTML('beforeend', html)
 })
+
+socket.on('locationMessage', (url) => {
+  console.log(url)
+  const html = Mustache.render(locationMessageTemplate, {
+    url
+  })
+  $messages.insertAdjacentHTML('beforeend',html)
+})
+
 
 $messageForm.addEventListener('submit', (e)=> {
   e.preventDefault()
