85cf15a1581629deda14e783aaf035ea89503872
Pawan Jain <jainpawan211199@gmail.com>
2020-01-02 13:40:51 +0530

    Event Acknowledgements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const path  = require('path')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -20,17 +21,26 @@ io.on('connection', (socket)=> {
   socket.emit('message', 'Welcome!')
   socket.broadcast.emit('message', 'A new user has joined!')
 
-  socket.on('sendMessage', (message)=> {
+  socket.on('sendMessage', (message, callback)=> {
+  const filter = new Filter()
+
+  if(filter.isProfane(message)){
+    return callback('Profanity Not Allowed')
+  }
+
     io.emit('message', message)
+    callback('Delivered')
+  })
+
+  socket.on('sendLocation', (coords, callback)=>{
+    io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    callback()
   })
 
   socket.on('disconnect', ()=>{
     io.emit('message', 'A user has left')
   })
 
-  socket.on('sendLocation', (coords)=>{
-    io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
-  })
 })
 
 server.listen(port, () => {
