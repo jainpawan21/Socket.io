4d1f5c566d8aadd689aa067a2928d499f51b6f00
Pawan Jain <jainpawan211199@gmail.com>
2020-01-02 12:12:25 +0530

    added concurrency

--- src/index.js
+++ src/index.js
@@ -13,17 +13,19 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0;
+
+// let count = 0;
 io.on('connection', (socket)=> {
   console.log('New WebSocket Connection')
 
-  socket.emit('countUpdated', count)
+  socket.emit('message', 'Welcome!')
+  // socket.emit('countUpdated', count)
 
-  socket.on('increment', () => {
-    count++
-    // socket.emit('countUpdated', count)
-    io.emit('countUpdated', count)
-  })
+  // socket.on('increment', () => {
+  //   count++
+  //   // socket.emit('countUpdated', count)
+  //   io.emit('countUpdated', count)
+  // })
 })
 
 server.listen(port, () => {
